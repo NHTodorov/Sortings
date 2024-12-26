c1290de771a9a7cbe67c05ea4e7a561b46bdf67d
NHTodorov <icot69@abv.bg>
2024-12-26 04:21:46 +0200

    Add files via upload

---
+++ Selection Sort/Selection Sort/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+void SelectionSort(int arr[], int num) {
+	for (int i = 0; i < num - 1 ; i++) {	// traversing entire array
+		int MinIndex = i;	// min element index
+
+		for (int j = 0; j < num - i - 1; j++) {	// next ellement of array to check
+			if (arr[j] < arr[MinIndex]) {	
+				//Note:	// for ascending order ---> arr[j] > arr[j + 1]
+
+				MinIndex = j;
+			}
+		}
+		swap(arr[i], arr[MinIndex]);	// swap the ellements if arr[j] < arr[MinIndex]
+										// or swap the ellements (ONLY when the Note is used)  if  arr[j] > arr[MinIndex]		
+	}
+}
+
+void CoutArr(int arr[], int num) {
+	for (int i = 0; i < num; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+int main() {
+	cout << "Insert the amount of numbers of ellements for sorting: " << endl;
+	int num;
+	cin >> num;
+	cout << "Insert ellements for sorting: " << endl;
+	int *arr = new int[num];	// dynamic array for cin ellements
+	for (int i = 0; i < num; i++) {
+		cin >> arr[i];
+	}
+	cout << endl;
+
+	cout << "Entered digits for sorting: " << endl;
+	CoutArr(arr, num);	// call function to show all of the inserted ellements for sorting
+	cout << endl;
+
+	SelectionSort(arr, num);	// call function to make selections
+
+	cout << "Sorted array: " << endl; 
+	CoutArr(arr, num);	// call function of sorted array 
+	cout << endl;
+
+	delete[] arr;	// delete the dynamic array
+
+	return 0;
+}
